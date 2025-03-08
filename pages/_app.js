7ef613c36ee7859ba37020cefaf613a24d8028fd
mohammad7979salehi@gmail.com <mohammad7979salehi@gmail.com>
2025-03-09 00:09:32 +0330

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Header from "../components/Header/Header";
+import { useRouter } from 'next/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const router = useRouter();
+  const showHeader = router.pathname !== '/';
+  return (
+    <>
+      {showHeader && <Header />}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
